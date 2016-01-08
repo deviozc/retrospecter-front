49f65d4a009ce1a0f4d419bb9eb162850e3c369d
Chen, William <William.Chen@move.com>
2016-01-08 10:33:16 -0800

    Add menu toggle

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,25 +7,31 @@ let yeomanImage = require('../images/yeoman.png');
 let ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 class AppComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {menuIsOpen: false};
+  }
+  openMenu(e) {
+    e.preventDefault();
+    this.setState({menuIsOpen: true});
+  }
+  closeMenu(e) {
+    e.preventDefault();
+    this.setState({menuIsOpen: false});
+  }
   render() {
     return (
       <div className="body">
         <div id="nav-bar"></div>
-        <a id="menu-toggle" href="#" className="btn btn-dark btn-lg toggle"><i className="fa fa-bars"></i></a>
-        <nav id="sidebar-wrapper">
+        <a id="menu-toggle" href="#" onClick={this.openMenu.bind(this)} className="btn btn-dark btn-lg toggle"><i className="fa fa-bars"></i></a>
+        <nav id="sidebar-wrapper" className={this.state.menuIsOpen ? "":"hidden"}>
           <ul className="sidebar-nav">
-            <a id="menu-close" href="#" className="btn btn-light btn-lg pull-right toggle"><i className="fa fa-times"></i></a>
+            <a id="menu-close" href="#" onClick={this.closeMenu.bind(this)} className="btn btn-light btn-lg pull-right toggle"><i className="fa fa-times"></i></a>
             <li className="sidebar-brand">
-              <a href="#top">Start Tool</a>
-            </li>
-            <li>
-              <a href="#top">Start</a>
+              <a href="/">Home</a>
             </li>
             <li>
               <Link to="/teams">Teams</Link>
-            </li>
-            <li>
-              <a href="#summary">Summary</a>
             </li>
             <li>
               <a href="#footer">Credit</a>
