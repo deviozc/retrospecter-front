59ab2d6c496541cd60aa2b5b7dbac07175971efe
Limian Wang <limian.wang@move.com>
2016-01-07 18:01:00 -0800

    fix alignment

--- src/components/Index.js
+++ src/components/Index.js
@@ -7,17 +7,15 @@ class IndexComponent extends React.Component {
   render() {
     return (
       <header id="top" className="header">
-    <div className="text-vertical-center">
-        <div className="highlight-div-primary">
+        <div className="text-vertical-center">
+          <div className="highlight-div-primary">
             <h1>Retrospecter</h1>
-
             <h3>Interactive Retrospective Tool</h3>
             <br />
             <a href="#teams" className="btn btn-dark btn-lg">Get Started</a>
+          </div>
         </div>
-    </div>
-
-</header>
+      </header>
     );
   }
 }
