5da5cc2316801267128c2765981afe8f56223d67
Chen, William <William.Chen@move.com>
2016-01-07 21:44:50 -0800

    Update style of create dialog

--- src/components/CreateTeamButton.js
+++ src/components/CreateTeamButton.js
@@ -8,7 +8,14 @@ import teamConstants from '../constants/team-constants';
 import teamAction from '../actions/TeamAction';
 import Modal from 'react-modal';
 import ReactDOM from 'react-dom';
-
+const customStyles = {
+  content : {
+    width                 : '600px',
+    margin                : '0 auto',
+    padding               : '0',
+    border                : '0'
+  }
+};
 class CreateTeamButtonComponent extends React.Component {
   constructor(props) {
     super(props);
@@ -38,16 +45,29 @@ class CreateTeamButtonComponent extends React.Component {
                     </span></button>
         <Modal
           isOpen={this.state.modalIsOpen}
-          onRequestClose={this.closeModal.bind(this)} >
+          onRequestClose={this.closeModal.bind(this)} style={customStyles}>
+              <div className="modal-dialog" role="document">
+                  <div className="modal-content">
+                      <div className="modal-header">
+                          <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={this.closeModal.bind(this)}><span
+                                  aria-hidden="true">&times;</span></button>
+                          <h4 className="modal-title" id="myModalLabel">Add Team</h4>
+                      </div>
+                      <form onSubmit={this.handleSubmit.bind(this)}>
+                          <div className="modal-body">
+                              <div className="form-group">
+                                      <label for="boardName">Team Name</label>
+                                      <input className="form-control" name="name" ref="name" type="text" placeholder="Board Name" />
+                                  </div>
+                          </div>
+                          <div className="modal-footer">
+                              <button type="button" className="btn btn-default" data-dismiss="modal" onClick={this.closeModal.bind(this)}>Cancel</button>
+                              <button type="button" className="btn btn-primary" type="submit">Create Team</button>
+                          </div>
+                      </form>
 
-          <h2>Hello</h2>
-          <button onClick={this.closeModal.bind(this)}>close</button>
-          <div>I am a modal</div>
-          <form onSubmit={this.handleSubmit.bind(this)}>
-            <input className="form-control" name="name" ref="name" type="text" />
-            <button type="submit">Submit</button>
-
-          </form>
+                  </div>
+              </div>
         </Modal>
       </div>
     );
